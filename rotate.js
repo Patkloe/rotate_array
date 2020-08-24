541196b2721217d50846005de54de2e158f54ee6
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-08-24 09:17:58 -0500

    Update rotate.js

--- rotate.js
+++ rotate.js
@@ -1,6 +1,14 @@
 rotate_array = () =>{
-var tab = [1,2,3,4,5,6,7];
-tab.unshift(tab.pop());
-return tab;
+var nbre = 0;
+var tab = [3,2,1,0,8,5,6,7];
+ for(var i = 0; i < tab.length; i++){
+  if(tab[i] === 0){
+     var keep = i;
+     nbre = tab.length - keep;
+  }
+ }
+ for(var j = 0; j < nbre -1; j++) 
+      tab.unshift(tab.pop());
+ return tab;
 }
 console.log(rotate_array());
